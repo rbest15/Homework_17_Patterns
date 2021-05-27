74b360377e928992a36aa0edbcb43d9b09fc9d41
moremist <rbest15@rambler.ru>
2021-05-27 12:49:53 +0300

    Initial Commit

---
+++ Homework_17_Patterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_17_Patterns
+//
+//  Created by Roman Kuzmich on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
